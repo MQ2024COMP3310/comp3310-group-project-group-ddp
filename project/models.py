b3e85fbb3a26d9db90fc365b8cf89be47fb99c35
Desert-SYS <80013627+Desert-SYS@users.noreply.github.com>
2024-06-01 19:31:59 +1000

    Comments
    Display comments working. Adding comments not yet ready.

--- project/models.py
+++ project/models.py
@@ -1,30 +1,47 @@
+
 from flask_login import UserMixin
 from . import db
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    photo_id = db.Column(db.Integer, db.ForeignKey('photo.id'), nullable=False)
+    
+ 
+
+    user = db.relationship('User', backref='comments')
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'photo_id': self.photo_id,
+            'comment': self.comment,
+        }
 
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     caption = db.Column(db.String(250), nullable=False)
     file = db.Column(db.String(250), nullable=False)
-    description = db.Column(db.String(600), nullable=True)
-    comment = db.Column(db.String(600), nullable=True)
+    description = db.Column(db.String(600), nullable=True) 
+    comments = db.relationship('Comment', backref='photo', lazy=True)
 
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-            'id': self.id,
-            'name': self.name,
-            'caption': self.caption,
-            'file': self.file,
-            'desc': self.description,
+        'id': self.id,
+        'name': self.name,
+        'caption': self.caption,
+        'description': self.description,
+        'file': self.file,
+        'comments': [comment.serialize for comment in self.comments],
         }
-
-
-class User(UserMixin, db.Model):
-    # primary keys are required by SQLAlchemy
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
